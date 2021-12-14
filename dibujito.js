cd3569445896cd5624081de1a329c41ad8f4d62c
yeixon2020 <yapinzon26@misena.edu.co>
2021-12-14 10:09:09 -0500

    Se realizaron nuevos cambios a los archivos nuevos de platzi

---
+++ dibujito.js
@@ -0,0 +1,19 @@
+var d =document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var l = 0;
+
+while(l < lineas)
+{
+    dibujarLinea("#AAF", 0, 0, 10, 300);
+}
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}    
+
