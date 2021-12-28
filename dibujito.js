11fe9106da246d79bf7c170d1eb3aa3dab1cf114
yeixon2020 <yapinzon26@misena.edu.co>
2021-12-28 11:26:10 -0500

    cambio realizado

--- dibujito.js
+++ dibujito.js
@@ -1,11 +1,19 @@
 var d =document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
 var l = 0;
+var yi, xf;
+var colorcito = "#FAA"
 
-while(l < lineas)
+for( l= 0; l < lineas; l++)
 {
-    dibujarLinea("#AAF", 0, 0, 10, 300);
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea "= + 1);
 }
+dibujarLinea(colorcito, 1,1,1,299);
+dibujarLinea(colorcito, 1,299,299,299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
